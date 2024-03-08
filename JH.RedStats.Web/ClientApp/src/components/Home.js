9bbda25b4a838c385ba00ae9fad1267f415f3914
mohghaderi <mohghaderi@gmail.com>
2024-03-08 12:59:11 -0600

    add simple UI to display data

--- JH.RedStats.Web/ClientApp/src/components/Home.js
+++ JH.RedStats.Web/ClientApp/src/components/Home.js
@@ -1,26 +1,105 @@
 import React, { Component } from 'react';
+import _ from "lodash"
 
 export class Home extends Component {
   static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { postsUpVotes: [], userPosts: [], loading: true };
+        this.loadingTimer = 0
+    }
+
+    componentDidMount() {
+        this.populateHomeDataDummy().then(r => console.log("stats updated"));
+        // this.loadingTimer = setIntertval(() => {
+        //     this.populateHomeData()
+        // }, 1000)
+    }
+
+    async populateHomeDataDummy() {
+        const data =  {
+            postsUpVotes: [{id: "123", title: "Post 1", count: 100}, {id: "124", title: "Post 2", count: 200}, {id: "124", title: "Post 2", count: 300}],
+            userPosts: [{id: "847", name: "user1", count: 200}, {id: "847", name: "user 2", count: 300}]
+        }
+        this.setState({ postsUpVotes: data.postsUpVotes, userPosts : data.userPosts, loading: false });
+    }
+
+    async populateHomeData() {
+        try {
+            const response = await fetch('homeStats');
+            const data = await response.json();
+            this.setState({ postsUpVotes: data.postsUpVotes,userPosts : data.userPosts, loading: false });
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    
+    static renderPostUpVotesTable(postsUpVotes) {
+        if (!_.isArray(postsUpVotes)) return (<div></div>)
+
+        return (<table className="table table-striped" aria-labelledby="tableLabel">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Up Votes Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            {postsUpVotes.map(p =>
+                <tr key={p.id}>
+                    <td>{p.title}</td>
+                    <td>{p.count}</td>
+                </tr>
+            )}
+            </tbody>
+        </table>)
+    }
+    
+    static renderUserPostsCount(userPosts) {
+        if (!_.isArray(userPosts)) return (<div>No data found!</div>)
+
+        return (<table className="table table-striped" aria-labelledby="tableLabel">
+            <thead>
+            <tr>
+                <th>User Name</th>
+                <th>Posts Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            {userPosts.map(p =>
+                <tr key={p.id}>
+                    <td>{p.name}</td>
+                    <td>{p.count}</td>
+                </tr>
+            )}
+            </tbody>
+        </table>)
+    }
+
+    render() {
+        if (this.state.loading) return <p><em>Loading...</em></p>
+
+        const postUpVotesTable = Home.renderPostUpVotesTable(this.state.postsUpVotes)
+        const userPostsTable = Home.renderUserPostsCount(this.state.userPosts)
+
+        return (
+            <div>
+                <h1>Reddit Statistics</h1>
+
+                <hr/>
+
+                <div className={"row"}>
+                <div className={"col col-6"}>
+                    <h2>Posts with most up votes</h2>
+                    {postUpVotesTable}
+                </div>
+                <div className={"col col-6"}>
+                    <h2>Users with most posts</h2>
+                    {userPostsTable}
+                </div>
+                </div>
+            </div>
+        );
+    }
 }
